0e2bba5232d9c4ef985a0c380d05a1779a6cb7ad
unutranyholas <unutranyholas@gmail.com>
2016-06-27 22:51:12 +0300

    Update boilerplate

---
+++ src/Components/Item.elm
@@ -0,0 +1,112 @@
+module Components.Item exposing (Model, Msg, init, update, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import List exposing (concat)
+
+
+-- import Html.Events exposing (..)
+--
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , color : String
+    , isActive : Bool
+    }
+
+
+init : String -> Model
+init name =
+    { name = name
+    , color = "#660000"
+    , isActive = True
+    }
+
+
+
+-- UPDATES
+
+
+type Msg
+    = Rename String
+    | Activate
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Rename name ->
+            { model | name = name }
+
+        Activate ->
+            { model | isActive = True }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    case model.isActive of
+        False ->
+            div [ class "entity-item", onClick Activate ]
+                [ i [ style [ ( "background-color", model.color ), ( "border-color", model.color ) ] ] []
+                , text model.name
+                ]
+
+        True ->
+            div [ class "entity-item active", style [ ( "background-color", model.color ) ] ]
+                [ i [ style [ ( "background-color", model.color ) ] ] []
+                , input [ type' "text", style inputStyle, value model.name, onInput Rename ] []
+                ]
+
+
+
+-- STYLES
+
+
+type alias Color =
+    String
+
+
+type alias Attributes =
+    List ( String, String )
+
+
+basicItem : Attributes
+basicItem =
+    [ ( "display", "inline-block" )
+    , ( "fontSize", "14px" )
+    , ( "fontFamily", "Helvetica, Arial, sans-serif" )
+    ]
+
+
+activeItem : Color -> Attributes
+activeItem color =
+    concat
+        [ basicItem
+        , [ ( "backgroundColor", color ) ]
+        ]
+
+
+defaultItem : Color -> Attributes
+defaultItem color =
+    concat
+        [ basicItem
+        , [ ( "color", color ) ]
+        ]
+
+
+inputStyle : Attributes
+inputStyle =
+    [ ( "border", "none" )
+    , ( "background", "transparent" )
+    , ( "color", "#fff" )
+    , ( "padding", "0px" )
+    , ( "fontSize", "inherit" )
+    , ( "fontFamily", "inherit" )
+    ]
--- src/Main.elm
+++ src/Main.elm
@@ -1,63 +1,247 @@
+module Main exposing (..)
+
+import Components.Item as Item exposing (..)
+import Html.App as App
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.App as Html
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Html.Events exposing (..)
+import List exposing (map, filter, head, concat, member, reverse)
+import Debug exposing (log)
 
 
--- APP
 main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    App.beginnerProgram
+        { model = init
+        , update = update
+        , view = view
+        }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    { items : List IndexedItem
+    , uid : Int
+    }
+
+
+type alias Id =
+    Int
+
+
+type alias IndexedItem =
+    { id : Id
+    , children : List Id
+    , model : Item.Model
+    }
+
+
+init : Model
+init =
+    { items = []
+    , uid = 0
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = AddParent
+    | AddSibling
+    | AddChild
+    | Remove
+    | Modify Id Item.Msg
+
 
 update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+update message ({ items, uid } as model) =
+    let
+        active =
+            getId (getActive items)
+
+        activeChildren =
+            getChildren (getActive items)
+
+        parent =
+            getId (getParent active items)
+    in
+        case message of
+            AddParent ->
+                { model
+                    | items =
+                        [ IndexedItem uid
+                            [ active ]
+                            (Item.init ("New entity" ++ toString uid))
+                        ]
+                            ++ map (replaceChildren parent [ active ] [ uid ]) items
+                    , uid = uid + 1
+                }
+
+            AddSibling ->
+                { model
+                    | items =
+                        [ IndexedItem uid
+                            []
+                            (Item.init ("New entity" ++ toString uid))
+                        ]
+                            ++ map (addChildren parent [ uid ]) items
+                    , uid = uid + 1
+                }
+
+            AddChild ->
+                { model
+                    | items =
+                        [ IndexedItem uid
+                            []
+                            (Item.init ("New entity" ++ toString uid))
+                        ]
+                            ++ map (addChildren active [ uid ]) items
+                    , uid = uid + 1
+                }
+
+            Remove ->
+                { model
+                    | items =
+                        items
+                            |> filter (\l -> not (l.id == active))
+                            |> map (addChildren parent activeChildren)
+                    , uid = uid + 1
+                }
+
+            Modify id msg ->
+                { model | items = map (updateHelp id msg) items }
+
+
+updateHelp : Id -> Item.Msg -> IndexedItem -> IndexedItem
+updateHelp targetId msg { id, children, model } =
+    IndexedItem id
+        children
+        (if targetId == id then
+            Item.update msg model
+         else
+            { model | isActive = False }
+        )
+
+
+
+-- removeItem : Id -> List IndexedItem -> List IndexedItem
+-- removeItem targetId items =
+
+
+addChildren : Id -> List Id -> IndexedItem -> IndexedItem
+addChildren targetId newChildren { id, children, model } =
+    IndexedItem id
+        (if targetId == id then
+            children ++ newChildren
+         else
+            children
+        )
+        { model | isActive = False }
+
+
+replaceChildren : Id -> List Id -> List Id -> IndexedItem -> IndexedItem
+replaceChildren targetId oldChildren newChildren { id, children, model } =
+    IndexedItem id
+        (if targetId == id then
+            exclude children oldChildren ++ newChildren
+         else
+            children
+        )
+        { model | isActive = False }
+
+
+getActive : List IndexedItem -> Maybe IndexedItem
+getActive items =
+    items
+        |> filter (\l -> l.model.isActive)
+        |> head
+
+
+getParent : Id -> List IndexedItem -> Maybe IndexedItem
+getParent id items =
+    items
+        |> filter (\l -> member id l.children)
+        |> head
+
+
+getId : Maybe IndexedItem -> Id
+getId item =
+    case item of
+        Nothing ->
+            -1
+
+        Just item ->
+            item.id
+
+
+getChildren : Maybe IndexedItem -> List Id
+getChildren item =
+    case item of
+        Nothing ->
+            []
+
+        Just item ->
+            item.children
+
+
+exclude : List Id -> List Id -> List Id
+exclude full part =
+    filter (\l -> not (member l part)) full
+
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
+    let
+        addParent =
+            button [ onClick AddParent ] [ text "Add Parent" ]
+
+        addSibling =
+            button [ onClick AddSibling ] [ text "Add Sibling" ]
+
+        addChild =
+            button [ onClick AddChild ] [ text "Add Child" ]
+
+        remove =
+            button [ onClick Remove ] [ text "Remove" ]
+
+        tree =
+            viewBranches model.items (findRoots model.items)
+    in
+        div [] ([ addParent, addSibling, addChild, remove ] ++ [ div [ class "entities-menu" ] [ tree ] ])
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+findRoots : List IndexedItem -> List Id
+findRoots items =
+    let
+        allIds =
+            map (\l -> l.id) items
+
+        allChildren =
+            items
+                |> map (\l -> l.children)
+                |> concat
+    in
+        exclude allIds allChildren
+
+
+viewBranches : List IndexedItem -> List Id -> Html Msg
+viewBranches items ids =
+    let
+        selectedItems =
+            filter (\l -> member l.id ids) items
+    in
+        case ids of
+            [] ->
+                text ""
+
+            _ ->
+                span [] (map (\l -> div [ class "level" ] [ App.map (Modify l.id) (Item.view l.model), viewBranches items l.children ]) (reverse selectedItems))
